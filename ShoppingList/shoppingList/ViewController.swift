5f13c96ee03faf8a4cefaf85cf2c55fa63267b65
Mohan <Mohan@inctures-macbook-air-2.local>
2018-04-24 14:58:34 +0530

    added firebase plist file, created custom table view cell

--- ShoppingList/shoppingList/ViewController.swift
+++ ShoppingList/shoppingList/ViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listItemTableViewCell", for: indexPath) as! listItemTableViewCell
+
+        return cell
+        
+    }
+    
+
+    
+    @IBOutlet weak var shopListTable: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.shopListTable.delegate = self
+        self.shopListTable.dataSource = self
+//        shopListTable.register(UINib(nibName: "listItemTableViewCell", bundle: nil), forCellReuseIdentifier: "listItem")
+        let nib = UINib.init(nibName: "listItemTableViewCell", bundle: nil)
+        shopListTable.register(nib, forCellReuseIdentifier: "listItemTableViewCell")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
---
+++ ShoppingList/shoppingList/listItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  listItemTableViewCell.swift
+//  shoppingList
+//
+//  Created by Mohan on 24/04/18.
+//  Copyright © 2018 Mohan. All rights reserved.
+//
+
+import UIKit
+
+class listItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
